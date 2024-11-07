bcd1fdd614b3b8a7cdc2791a9832190ddfbf85c0
chinmay768 <chinmaysahani07@gmail.com>
2024-11-07 22:08:19 +0530

    Linked List

---
+++ src/striver/LinkedList.java
@@ -0,0 +1,29 @@
+package striver;
+
+public class LinkedList {
+
+    public static void printLL(Node head){
+        while (head != null){
+            System.out.println(head.data);
+            head = head.next;
+        }
+    }
+
+    public static Node convertArrayToLL(int[] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i = 1; i < arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = mover.next;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        Node head = convertArrayToLL(arr);
+
+        printLL(head);
+    }
+}
---
+++ src/striver/Node.java
@@ -0,0 +1,15 @@
+package striver;
+
+public class Node {
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+    }
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+}
