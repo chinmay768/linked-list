64176b9faf14a8b19005c9df30c14ce3f8cf717c
chinmay768 <chinmaysahani07@gmail.com>
2024-11-25 22:52:51 +0530

    Flatten LL

--- src/striver/FlatteningALLWC.java
+++ src/striver/FlatteningALLWC.java
@@ -27,24 +27,35 @@ public class FlatteningALLWC {
         return flatHead;
     }
 
-//    public static NodeWC merge(NodeWC head1, NodeWC head2){
-//        NodeWC dummyNode = new NodeWC(-1);
-//        NodeWC res = dummyNode;
-//        while (head1 != null && head2 != null){
-//            if(head1.data < head2.data){
-//                res.child = head1;
-//                res = head1;
-//                head1 = head2.child;
-//            }
-//        }
-//    }
-//
-//    public static NodeWC flatten(NodeWC head){
-//        if(head == null || head.next == null) return head;
-//
-//        NodeWC mergedHead = merge(head.next);
-//
-//    }
+    public static NodeWC merge(NodeWC head1, NodeWC head2){
+        NodeWC dummyNode = new NodeWC(-1);
+        NodeWC res = dummyNode;
+        while (head1 != null && head2 != null){
+            if(head1.data < head2.data){
+                res.child = head1;
+                res = head1;
+                head1 = head1.child;
+            }else {
+                res.child = head2;
+                res = head2;
+                head2 = head2.child;
+            }
+            res.next = null;
+        }
+
+        if(head1 != null) res.child = head1;
+        if(head2 != null) res.child = head2;
+
+        return dummyNode.child;
+    }
+
+    public static NodeWC flatten(NodeWC head){
+        if(head == null || head.next == null) return head;
+
+        NodeWC mergedHead = flatten(head.next);
+        return merge(head, mergedHead);
+
+    }
 
     public static void main(String[] args) {
         NodeWC head = new NodeWC(3);
@@ -65,7 +76,7 @@ public class FlatteningALLWC {
 
 
 
-        head = flattenNaive(head);
+        head = flatten(head);
         LinkedListWithChild.printLLWC(head);
     }
 }
